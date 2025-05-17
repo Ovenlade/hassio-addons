591055d1e499b548982846544986b345643ccdfa
Ovenlade <139609087+Ovenlade@users.noreply.github.com>
2025-05-18 01:01:12 +0200

    Update run.sh

--- mediatracker/run.sh
+++ mediatracker/run.sh
@@ -1,9 +1,2 @@
-#!/usr/bin/env bash
-set -e
-
-PORT=${PORT:-4020}
-
-echo "✅ MediaTracker wordt gestart op poort $PORT..."
-
-# Start MediaTracker
-npm start -- --port $PORT
+#!/usr/bin/env sh
+exec /app/mediatracker --port ${PORT:-4020}
