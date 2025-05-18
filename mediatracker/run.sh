f59545f6d7c7bba81e7f24069b57541a66504e85
Ovenlade <139609087+Ovenlade@users.noreply.github.com>
2025-05-18 10:55:35 +0200

    Update run.sh

--- mediatracker/run.sh
+++ mediatracker/run.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+# Maak de map aan als die nog niet bestaat
+if [ ! -d "/data/mediatracker" ]; then
+    mkdir -p /data/mediatracker
+    echo "Directory /data/mediatracker aangemaakt."
+fi
+
+
 # Start MediaTracker op de gewenste poort en sla data op in /data
-/app/MediaTracker --host 0.0.0.0 --port 7481 --dataPath /addon_configs/mediatracker
+/app/MediaTracker --host 0.0.0.0 --port 7481 --storage /data/mediatracker
